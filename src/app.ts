f75c6012caebf3b1e55131aac0f8defb468dc980
Sankalp Sharma <sansharma056@gmail.com>
2021-11-05 20:59:59 +0530

    added build script

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import http from "http";
+import * as http from "http";
 import { Server } from "socket.io";
 
 type Message =
@@ -21,7 +21,7 @@ const server = http.createServer();
 
 const io = new Server(server, {
 	cors: {
-		origin: `http://localhost:1234`,
+		origin: process.env.ORIGIN_URL || "http://localhost:1234",
 		methods: ["GET", "POST"],
 	},
 });
